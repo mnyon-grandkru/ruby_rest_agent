d44a41d53371de9d35593f983c2e6e1d5808d1ae
Mark Nyon <msnyon@hey.com>
2023-10-05 07:37:04 -0400

    on #14: made game_date a class method

--- lib/ruby_rest_agent/utils.rb
+++ lib/ruby_rest_agent/utils.rb
@@ -3,7 +3,7 @@
 # - Utility methods
 #
 class RubyRestAgent::Utils
-  def game_date(selected_date = nil)
+  def self.game_date(selected_date = nil)
     formatted_selected_date = DateTime.parse(selected_date.to_s).strftime("%Y%m%d")
     puts("selected date #{formatted_selected_date}")
     formatted_selected_date
--- spec/ruby_rest_agent/utils_spec.rb
+++ spec/ruby_rest_agent/utils_spec.rb
@@ -2,18 +2,18 @@
 
 require 'spec_helper'
 
-describe RubyRestAgent::Utils, '#game_date' do
+describe RubyRestAgent::Utils, '.game_date' do
   describe 'on success' do
     let(:yesterday) { Date.today - 1 }
     let(:random_date) { Date.new(2002, 6, 24) }
-    let(:utils_date) { described_class.new }
+
 
     it 'returns date in a format "YYYYMMDD" for yesterday with no arguments' do
-      expect(utils_date.game_date(yesterday)).to eq(yesterday.strftime('%Y%m%d'))
+      expect(RubyRestAgent::Utils.game_date(yesterday)).to eq(yesterday.strftime('%Y%m%d'))
     end
 
     it 'returns date in a format "YYYYMMDD" for another date with no arguments' do
-      expect(utils_date.game_date(random_date)).to eq(random_date.strftime('%Y%m%d'))
+      expect(RubyRestAgent::Utils.game_date(random_date)).to eq(random_date.strftime('%Y%m%d'))
     end
   end
 end
